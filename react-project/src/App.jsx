2aa6b00eea9ba8329b012064bbac16b20ca04ad1
Fizza <162970932+fizzala@users.noreply.github.com>
2025-09-11 13:01:21 +0500

    code added

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './components/navbar'
-
+import { BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Navbar/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </Router>
     </>
   )
 }
-
 export default App
